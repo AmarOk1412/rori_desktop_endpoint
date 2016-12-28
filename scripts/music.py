0c83faa453c81dd12966b6dbe95e13dc1707eb62
AmarOk <blin.sebastien@enconn.fr>
2016-12-28 17:29:38 -0500

    update music script

--- scripts/music.py
+++ scripts/music.py
@@ -4,7 +4,7 @@ import subprocess
 if len(sys.argv) > 1:
     if sys.argv[1] == "start":
         print("start music")
-        subprocess.Popen("rhythmbox \"`find ~/Music/*.mp3 -type f | shuf -n 1`\" &>/dev/null", shell=True)
+        subprocess.Popen("rhythmbox \"`find ~/Music/*.mp3 -type f | shuf -n 1`\"&", shell=True)
     if sys.argv[1] == "stop":
         print("stop music")
-        subprocess.Popen("pkill rhythmbox &>/dev/null", shell=True)
+        subprocess.Popen("pkill rhythmbox", shell=True)
